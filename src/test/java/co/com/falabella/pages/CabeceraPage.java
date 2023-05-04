4dffb13b4eab2cdb7ba219324a242d409b3d4f40
Maxtro2002 <jspr200231@gmail.com>
2023-05-04 12:38:21 -0500

    Eliminados ejemplos clase

--- src/test/java/co/com/falabella/pages/CabeceraPage.java
+++
@@ -1,9 +0,0 @@
-package co.com.falabella.pages;
-
-import net.serenitybdd.screenplay.targets.Target;
-import org.openqa.selenium.By;
-
-public class CabeceraPage {
-    public static Target BARRA_BUSQUEDA = Target.the("barra de busqueda").located(By.id("testId-SearchBar-Input"));
-
-}
--- src/test/java/co/com/falabella/pages/ResultadosBusquedaPage.java
+++
@@ -1,9 +0,0 @@
-package co.com.falabella.pages;
-
-import net.serenitybdd.screenplay.targets.Target;
-import org.openqa.selenium.By;
-
-public class ResultadosBusquedaPage {
-    public static Target BOTON_AGREGAR_CARRITO_PRIMER_ELEMENTO = Target.the("boton Agregar al carrito primer elemento").locatedBy("//div[@pod-layout='LIST'][1]//button[contains(@class,'button-primary')]");
-
-}
